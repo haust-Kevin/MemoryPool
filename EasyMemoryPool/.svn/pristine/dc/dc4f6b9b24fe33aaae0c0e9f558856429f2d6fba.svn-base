8d2940b36f7850ac281d7dbae97fe02860e63a89
定邦 王 <878474339@qq.com>
2020-08-02 23:42:56 +0800

    完成内存池初始化

---
+++ EasyMemoryPool/.svn/pristine/dc/dc4f6b9b24fe33aaae0c0e9f558856429f2d6fba.svn-base
@@ -0,0 +1,25 @@
+#include "MemoryAlloc.h"
+#include <cstdlib>
+
+MemoryAlloc::MemoryAlloc()
+{
+}
+
+MemoryAlloc::~MemoryAlloc()
+{
+}
+
+void* MemoryAlloc::alloc(size_t size)
+{
+	return malloc(size);
+}
+
+void MemoryAlloc::free(void* p)
+{
+
+}
+
+void MemoryAlloc::init()
+{
+
+}
--- EasyMemoryPool/MemoryAlloc.cpp
+++ EasyMemoryPool/MemoryAlloc.cpp
@@ -1,8 +1,12 @@
 #include "MemoryAlloc.h"
 #include <cstdlib>
+#include <exception>
+#include "MemoryBlock.h"
 
 MemoryAlloc::MemoryAlloc()
+	:pMemory(nullptr), pHeader(nullptr), blockNum(0), blockSize(0)
 {
+
 }
 
 MemoryAlloc::~MemoryAlloc()
@@ -21,5 +25,29 @@ void MemoryAlloc::free(void* p)
 
 void MemoryAlloc::init()
 {
-
+	if (pMemory != nullptr)
+	{
+		throw std::exception("内存池已经初始化\n");
+	}
+	size_t memorySize = blockSize * blockNum; // 计算内存池大小
+	pMemory = malloc(memorySize); // 向系统申请内存
+ 	pHeader = static_cast<MemoryBlock*> (pMemory);
+// 	pHeader->inPool = true;
+// 	pHeader->nId = 0;
+// 	pHeader->refCount = 0;
+// 	pHeader->pAlloc = this;
+// 	pHeader->pNext = nullptr;
+	MemoryBlock* pLast = pHeader;
+	for (size_t i = 1; i < blockNum; i++)
+	{
+		MemoryBlock* pTemp = 
+			reinterpret_cast<MemoryBlock*> (static_cast<char*>(pMemory) + (i * blockSize));
+		pTemp->inPool = true;
+		pTemp->nId = i;
+		pTemp->refCount = 0;
+		pTemp->pAlloc = this;
+		pLast->pNext = pTemp;
+		pLast = pTemp;
+	}
+	pLast->pNext = nullptr;
 }
