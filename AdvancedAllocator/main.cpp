9aa4220eea77d3cc2705ebba62c05ce208a8dec6
定邦 王 <878474339@qq.com>
2020-08-05 16:52:51 +0800

    完成AdvancedAllocator的项目创建，并构建整体框架

---
+++ AdvancedAllocator/main.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+
+#include "ALLOCATOR.HPP"
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+	vector<int, Kevin::allocator<int>> ivec;
+	return 0;
+}
--- EasyMemoryPool/main.cpp
+++ EasyMemoryPool/main.cpp
@@ -3,7 +3,8 @@
 #include <vector>
 #define MAX_BLOCK_SIZE 10240ULL
 #include "Allocator.h"
-//#include "MemoryPool.h"
+#include <chrono>
+
 using namespace std;
 
 template<size_t size>
@@ -18,26 +19,31 @@ private:
 
 int main(int argc, char* argv[])
 {
-	printf("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n");
- 	while (true)
- 	{
- 		auto p1 = new Demo<100>();
- 		auto p2 = new Demo<129>();
- 		auto p3 = new Demo<256>();
- 		auto p4 = new Demo<522>();
- 		auto p5 = new Demo<1024>();
- 		auto p6 = new Demo<2022>();
- 		auto p7 = new Demo<12022>();
- 		cout << "----" << endl;
- 		delete p1;
- 		delete p2;
- 		delete p3;
- 		delete p4;
- 		delete p5;
- 		delete p6;
- 		delete p7;
- 	}
-	new char[100];
-	return 0;
+    printf("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n");
+
+    int n = 1000000;
+    auto start = chrono::high_resolution_clock::now();
+    while (n--)
+    {
+        auto p1 = new Demo<100>;
+        auto p2 = new Demo<129>;
+        auto p3 = new Demo<256>;
+        auto p4 = new Demo<522>;
+        auto p5 = new Demo<1024>;
+        auto p6 = new Demo<2022>;
+        auto p7 = new Demo<10239>;
+        //cout << "----" << endl;
+        delete p1;
+        delete p2;
+        delete p3;
+        delete p4;
+        delete p5;
+        delete p6;
+        delete p7;
+        delete new Demo<10239>;
+    }
+    auto end = chrono::high_resolution_clock::now();
+    cout << chrono::nanoseconds(end - start).count() / (1000.0 * 1000 * 1000) << endl;
+    return 0;
 }
 
